3506eb4813e1616e186ab46bfe40897b8b68f369
Marius Drienik <marius@drienik.com>
2024-04-14 08:42:18 +0930

    neovim - adss fugitive to the configuration

--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -3,6 +3,6 @@ return {
     'mbbill/undotree',
 
     -- setup F5 to display the Undo tree
-    vim.keymap.set('n', '<leader><F5>', vim.cmd.UndotreeToggle),
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle),
   },
 }
